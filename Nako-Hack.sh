b154c0d8699c111ce3d7e9bd0b16f5e24dd083c8
Cr4bbyP4tty <47083843+Cr4bbyP4tty@users.noreply.github.com>
2019-04-28 00:52:14 +0700

    Create Nako-Hack.sh

---
+++ Nako-Hack.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+ 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+clear
+figlet "Nako_Coders"|lolcat
+echo ""
+echo $cyan "Decode by NakoCoders"
+echo $cyan "WA : +81968535845"
+echo $cyan "FB : NakoTan"
+echo $cyan "Notif : Nothing Give Up "
+echo $yellow "Hanya Bisa Digunakan 5x Per IP selain itu ga Work Ajg"
+sleep 5
+echo $white "Waiting ........."
+sleep 10
+echo $cyan " Masukkan Nama Target :"
+read kartu
+echo $cyan " Masukkan Nomor Target :"
+read anda
+echo "\033[0;32m]List Socks : "
+read socks
+echo $red "Loading ....."
+sleep 5
+echo $red "Waiting......."
+sleep 5
+echo $red "Sedang Tanem Backdoor ke $kartu"
+echo $red "system Sukses Tanem Backdoor 192.168.85.685:9685......"
+sleep 5
+echo $red "Sedang Mengirim Akses Backdoor ke $anda"
+sleep 3
+echo $red "Transfer Database ......"
+sleep 5
+echo $red "Socks $pulsa Reading List......"
+sleep 5
+echo "Loading...."
+sleep 3
+echo "1%"
+sleep 3
+echo "35%"
+sleep 3
+echo "50%"
+echo "60%"
+sleep 3
+echo "80%"
+sleep 3
+echo "90%"
+sleep 3
+echo "100%"
+sleep 5
+echo "Waiting........."
+sleep 1
+echo "Sukses Nigga......"
+sleep 1
+echo "Sukses Akses Backdoor,  Mengirim Ke : $anda"
+echo "Kode Verifikasi Wa nya : 685 584"
+echo "Terima Kasih ^_^"
